238b38840f6b726bcdc29eafa2a4cad32d3f6cb7
Aaron Sammut <b42594@mcast.org.mt>
2017-10-11 13:30:57 +0200

    This is the first lesson about unity. if statments, methods, variables.

---
+++ New Unity Project/Assets/Scripts/Exercise1.cs
@@ -0,0 +1,57 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Exercise1 : MonoBehaviour {
+    int number = 20;
+    int min = 0;
+    int max = 100;
+
+    void Check()
+    {
+        if(number <= min)
+        {
+            print("the number reached the minimum");
+            UnityEditor.EditorApplication.isPlaying = false;
+        }
+        if (number >= max)
+        {
+            print("the number reached the maximum");
+            UnityEditor.EditorApplication.isPlaying = false;
+        }
+    }
+
+	// Use this for initialization
+	void Start () {
+        print(number);
+        print("up = +1; down = -1; left = +10; right = -10");
+	}
+
+	// Update is called once per frame
+	void Update () {
+        if (Input.GetKeyDown(KeyCode.UpArrow))
+        {
+            number = number + 1;
+            print(number);
+            Check();
+        }
+        if (Input.GetKeyDown(KeyCode.DownArrow))
+        {
+            number = number - 1;
+            print(number);
+            Check();
+        }
+        if (Input.GetKeyDown(KeyCode.LeftArrow))
+        {
+            number = number + 10;
+            print(number);
+            Check();
+        }
+        if (Input.GetKeyDown(KeyCode.RightArrow))
+        {
+            number = number - 10;
+            print(number);
+            Check();
+        }
+    }
+}
---
+++ New Unity Project/Assets/Scripts/NumberWizard.cs
@@ -0,0 +1,63 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class NumberWizard : MonoBehaviour {
+    //Created global int variables 
+    int max = 1000;
+    int min = 1;
+    int guess = 500;
+    
+    void StartGame(){
+        //Prints these only once
+        print("Welcome to NumeberWizard");
+        Debug.Log("Think a number from " + min + " to " + max);
+        NextGuess();
+    }
+    void NextGuess() {
+        Debug.Log("Is the number greater than " + guess + "?");
+        Debug.Log("Up : Bigger, Down: Smaller, Return: Equel");
+    }
+
+    // Use this for initialization
+    void Start () {
+        StartGame();
+    }
+    
+	// Update is called once per frame
+	void Update () {
+        if (Input.GetKeyDown(KeyCode.UpArrow))
+        {
+            print("Up Arrow pressed");
+            min = guess;
+            guess = (min + max) / 2;
+            NextGuess();
+        }
+
+        //Checks if Down Arrow is pressed
+        else if (Input.GetKeyDown(KeyCode.DownArrow))
+        {
+            print("Down Arrow pressed");
+            max = guess;
+            guess = (min + max) / 2;
+            NextGuess();
+        }
+
+        //Checks if Return is pressed
+        else if (Input.GetKeyDown(KeyCode.Return))
+        {
+            print("You won!!!!");
+            print("Press P to play again");
+            print("Press Q to quit");
+
+            if (Input.GetKeyDown(KeyCode.P))
+            {
+                StartGame();
+            }
+            else if (Input.GetKeyDown(KeyCode.Q))
+            {
+                UnityEditor.EditorApplication.isPlaying = false;
+            }
+        }
+    }
+}
